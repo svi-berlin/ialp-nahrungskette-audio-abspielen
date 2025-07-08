40849b39ff60ea5ed8602a549583f9f55f773782
svi-berlin <svi-berlin@users.noreply.github.com>
2025-07-08 20:37:20 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    if (audiofile > anfang) {
+        audiofile += -1
+    } else {
+        audiofile = ende
+    }
+    serialmp3.playMp3Track(audiofile, audioordner)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    if (audiofile < ende) {
+        audiofile += 1
+    } else {
+        audiofile = anfang
+    }
+    serialmp3.playMp3Track(audiofile, audioordner)
+})
+let ende = 0
+let anfang = 0
+let audiofile = 0
+let audioordner = 0
+serialmp3.connectSerialMp3(DigitalPin.C16, DigitalPin.C17)
+serialmp3.setMp3Volume(30)
+let audioanzahl = 12
+audioordner = 4
+audiofile = 1
+anfang = 1
+ende = audioanzahl
+basic.forever(function () {
+	
+})
